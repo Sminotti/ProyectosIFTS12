da5fde15be47f2ab0113f6ebe20b34584bc02448
Sminotti <sebastianminotti@gmail.com>
2024-09-24 17:34:23 -0300

    funciona ok

---
+++ condicionales/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace condicionales
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int numero_1;
+            int numero_2;
+
+            Console.WriteLine("Ingrese dos numeros:");
+            Console.WriteLine("Escriba el primer numero:");
+            numero_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el primer numero:");
+            numero_2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero_1 == numero_2) { Console.WriteLine("{0}=={1}", numero_1, numero_2); }
+            if (numero_1 <= numero_2) { Console.WriteLine("{0}<={1}", numero_1, numero_2); }
+            if (numero_1 >= numero_2) { Console.WriteLine("{0}>={1}", numero_1, numero_2); }
+            if (numero_1 != numero_2) { Console.WriteLine("{0}!={1}", numero_1, numero_2); }
+            if (numero_1 < numero_2) { Console.WriteLine("{0}<{1}", numero_1, numero_2); }
+            if (numero_1 > numero_2) { Console.WriteLine("{0}>{1}", numero_1, numero_2); }
+        }
+    }
+}

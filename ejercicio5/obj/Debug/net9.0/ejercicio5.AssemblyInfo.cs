f945238e336407e04c6d3b6e78cffc05611af836
Sminotti <sebastianminotti@gmail.com>
2024-10-16 18:04:09 -0300

    Ejercicio 5 terminado

--- ejercicio5/obj/Debug/net9.0/ejercicio5.AssemblyInfo.cs
+++ ejercicio5/obj/Debug/net9.0/ejercicio5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68f95e5eba2313bdc70cc218d47ff5859ed21b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc98105d097f50948f9eb3238ae43fa4c15d1cbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
